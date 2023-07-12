308e5882f37f94572a9e50718602b0bce263485e
Dhruvam Zaveri <dhruvamzaveri14@gmail.com>
2023-07-12 18:25:29 +0530

    trying something new to delete large modules in node_modules

--- remove_large_modules.sh
+++ remove_large_modules.sh
@@ -2,7 +2,7 @@
 
 # ---------------------- Main () --------------------
 large_modules=$(du -sh ./node_modules/* | sort -nr | grep 'M.*' | awk '{print $2}')
-
+echo deleting $large_modules
 for module in ${large_modules[@]}
 do 
     rm -rvf $module
